233f4ced87fbf392066d9a2cac41af19bea48a3d
vitordanielrosa <143666152+vitordanielrosa@users.noreply.github.com>
2025-09-10 11:57:36 -0300

    Exercicio da padaria 2.0 melhorado

---
+++ modulo 2/aula12/padaria2.0.py
@@ -0,0 +1,106 @@
+def dados() -> dict:
+    """Carregar e retornar os dados de produtos, frete e funcionarios"""
+    return{
+        "atendente" : "Maria",
+        "paes":{
+            "frances":{"nome":"Pão frances", "Valor": 0.50,"Quantidade":15},
+            "Daces":{"nome":"Pão doce", "Valor": 5.00,"Quantidade":20},
+            "Forma":{"nome":"Pão de forma", "Valor": 5.99,"Quantidade":10},
+        },
+        "bairros":{ 
+            "barroco":{"nome":"Barroco","Frete":5.00},
+            "Sao jose": {"nome":"Sao jose", "Frete": 15.00},
+        },
+        "codigo_venda_base":95875
+    }
+def obter_dados_cliente() -> dict:
+    """Solicita e retorna os dados do cliente"""
+    nome = input("Informe seu nome:")
+    return {"nome": nome}
+def solicitar_forma_pagamento() -> str:
+    """Solicitar e retornar a forma de pagamento escolhida"""
+    while True:
+        pagamento = input("Escolha a forma de pagamento(1-Dinehiro, 2-Cartao)")
+        if pagamento =="1":
+            return"Dinheiro"
+        elif pagamento == "2":
+            return "Cartao"
+        else:
+            print("Forma de pagamento invalida")
+
+def gerar_codigo_venda(codigo_base:int) -> int:
+    """Gera e retorna o codigo de venda"""
+    return codigo_base + 1 
+
+def calcular_frete(bairros_disponiveis: dict) -> tuple[str, float] | None:
+    """Calcular o valor com base no bairro de entrega"""
+    print("Bairros para entrega")
+    while True:
+        for bairo in bairros_disponiveis.values():
+            print(f" - {bairo["nome"]}")
+            bairros_entrega_nome = input("Qual o bairro de entrega?").lower()
+            bairro_encontrado = ""
+            for chave, bairo in bairros_disponiveis.values():
+                if bairo["nome"].lower() == bairros_entrega_nome:
+                    bairro_encontrado = chave
+                    break
+            if not bairro_encontrado:
+                print("Bairro fora da area de Entrega")
+            else:
+                frete = bairros_disponiveis[bairro_encontrado]["frete"]
+                return bairros_entrega_nome, frete
+
+def cadastrar_produto (estoque:dict)-> None:
+    """Permite ao atende cadastrar um novo produto """
+    nome_produto = input("Digite o nome do novo produto(identificador)").lower().strip()
+
+    if nome_produto in estoque:
+        print("Error! Produto ja cadastrado com este identificador!!!")
+        return 
+    
+    try: 
+        nome_completo = input("Digite o nome completo do produto: ")
+        valor = input(float("Digite o valor do novo produto:"))
+        quantidade = int(input("Digite a quantidade inicial do produto:"))
+        
+        if nome_produto and valor > 0 and quantidade > 0:
+            estoque[nome_completo] = {"nome":nome_completo, "valor":valor,"quantidade":quantidade}
+            print(f"Produto{nome_completo} cadastrado com sucesso!")
+        
+        else:
+            print("Error! Dados invalidos.")
+    except ValueError:
+        print("Entrada de dados invalida")
+
+def atualizar_produto(estoque: dict) -> None:
+    """Permite ao atendente atualizar um produto exitente"""
+    nome_produto = input("Digite o nome do produto para atualizar (identificador): ").lower()
+
+    if nome_produto not in estoque:
+        print("Produto nao cadastrado")
+        return
+    
+    print(f"Produto '{estoque[nome_produto]}' selecionado.")
+    escolha = input("O que deseja atualizar? \n \
+          1- Valor; \n\
+          2 - quantidade")
+    
+    try:
+        if escolha == "1":
+            novo_valor = float(input("Digite o novo valor do produto:"))
+            if novo_valor > 0:
+                estoque[ nome_produto]["valor"] = novo_valor
+                print(f"Valor atualizado para R${novo_valor:.2f}")
+            else:
+                print("Valor invalido.")
+        elif escolha == "2":
+            nova_quantidade = int(input("Digite a nova quantidade do produto "))
+            if nova_quantidade > 0:
+                estoque[nome_produto]["quantidade"] += nova_quantidade
+                print(f"Quantidade atual de {estoque[nome_produto]["quantidade"]} itens.")
+            else:
+                print("Quantidade invalida!")
+        else:
+            print("Erro! Opção invalida.")
+    except ValueError:
+        print("Error! Digite apenas numeros")
