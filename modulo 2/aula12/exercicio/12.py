545314159f8694291c65c9a3909da980256b0996
vitordanielrosa <vitorcabral2004@gmail.com>
2025-09-22 16:50:59 -0300

    Feito. Utilizando o metodo str

---
+++ modulo 2/aula12/exercicio/12.py
@@ -0,0 +1,10 @@
+class Filme:
+    def __init__(self, titulo:str, diretor:str, ano:int):
+        self.titulo = titulo
+        self.diretor = diretor
+        self.ano = ano
+    def __str__(self):
+        return f"Filme: {self.titulo} ({self.ano}) - Diretor:{self.diretor}"
+
+fil = Filme("De Volta para o Futuro", "Robert Zemeckis", 1985)
+print(fil)
