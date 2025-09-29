63b50727d91ce0a673624aba78eee46c3dd27d2b
fabiolobo17 <143666152+vitordanielrosa@users.noreply.github.com>
2025-09-29 11:53:08 -0300

    exercicio de tipagem e lista

---
+++ modulo 2/aula16/exercicio 5.py
@@ -0,0 +1,17 @@
+class Comodo:
+    def __init__(self, nome):
+        self.nome = nome
+class Casa:
+    def __init__(self):
+        self.lista = []
+        self.como = Comodo()
+    def adicionar(self,):
+        cont = 0
+        m = self.lista.append({self.como : cont})
+        cont+=1
+        for i in self.lista:
+            print(f"O comodo e {i}")
+    
+Comodo("Au")
+m = Casa("Banheiro")
+m.adicionar("Casa")
